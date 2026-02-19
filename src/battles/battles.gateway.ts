1a17cf3e71293d280d74222874da79fea8cf7ae0
Ian <iaancastellanos1234@gmail.com>
2026-02-19 13:11:27 +0100

    implement joinBattle with room logic and logging

--- src/battles/battles.gateway.ts
+++ src/battles/battles.gateway.ts
@@ -7,10 +7,13 @@ import {
 import { Server, Socket } from 'socket.io';
 import { JwtService } from '@nestjs/jwt';
 import { WebsocketsService } from './websockets.servie';
+import { SubscribeMessage, MessageBody, ConnectedSocket } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common'; 
 
 @WebSocketGateway({ cors: { origin: '*' } })
 export class BattlesGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: Server;
+    private readonly logger = new Logger('BattlesGateway'); //
 
   constructor(
     private readonly jwtService: JwtService,
@@ -34,5 +37,29 @@ export class BattlesGateway implements OnGatewayConnection, OnGatewayDisconnect 
   handleDisconnect(client: Socket) {
     this.websocketsService.removeClient(client.id);
     console.log(`Clientes restantes: ${this.websocketsService.getConnectedClients()}`);
+  }
+
+  @SubscribeMessage('joinBattle')
+  handleJoinBattle(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() payload: { battleId: number }
+  ) {
+
+    this.logger.debug(`Evento joinBattle recibido para la batalla: ${payload.battleId}`);
+
+    if (!client) {
+      this.logger.error('El objeto client (socket) es undefined'); //
+      return { status: 'error', message: 'Socket no detectado' };
+    }
+
+    const roomName = `battle_${payload.battleId}`;
+    client.join(roomName);
+
+    this.logger.log(`Socket ${client.id} unido a ${roomName}`); //
+
+    return {
+      status: 'ok',
+      message: `Te has unido a la sala ${roomName}`
+    };
   }
 }
